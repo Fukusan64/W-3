411c9ba5b3663be799421f049eb2e06a002260b5
Fukusan64 <ikishikawa64@gmail.com>
2018-12-18 17:54:52 +0900

    static server

---
+++ index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.listen(3000, () => console.log('listening on http://localhost:3000'));
