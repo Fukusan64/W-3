94e72bb1563a407b431a054978f4dba26bdd77fc
Fukusan64 <ikishikawa64@gmail.com>
2018-12-19 18:43:45 +0900

    exec画面

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
+const editMode = () => {
+  document.getElementById('exec').style.display = 'none';
+  document.getElementById('editor').style.display = 'block';
+  document.getElementById('modeButton').innerText = '実行';
+};
+
+const execMode = () => {
+  document.getElementById('editor').style.display = 'none';
+  document.getElementById('exec').style.display = 'flex';
+  document.getElementById('modeButton').innerText = '編集';
+};
+
+const init = () => {
+  editMode();
+  const modeButton = document.getElementById('modeButton')
+  modeButton.addEventListener('click', () => {
+    if (modeButton.innerText == '編集') editMode();
+    else execMode();
+  })
+}
+
 window.onload = () => {
-	
+  init();
 };
