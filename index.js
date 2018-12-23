c20281f987ef270e2baa9df2b89953384a37a8ab
Fukusan64 <ikishikawa64@gmail.com>
2018-12-23 20:35:23 +0900

    ちょっと構文エラーを判定できるようにした

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const deltaTmsec = 100;
 
 const executer = new Executer([1,2,3,4], deltaTmsec);
 
+const naturalNumberTest = str => /^[0-9]+$/.test(str);
+const numberTest = str => /^-?[0-9]+$/.test(str);
+const pinTest = str => /^pin[1-9]$/.test(str);
+
 const parser = (text) => {
   const cmdArr = []
   text
@@ -26,8 +30,18 @@ const parser = (text) => {
     .map(e => {
       return e.replace(/button[0-9]/, '').trim().split('\n').map(e => {
         const [pinData, sec] = e.split(':');
+        if (!naturalNumberTest(sec)) {
+          throw `naturalNumberTest error (sec): ${sec}`;
+        }
         const data = {sec: Number(sec)};
         pinData.split(',').map(e => e.split('=')).forEach(e => {
+          const pin = e[0].trim();
+          if (!pinTest(pin)) {
+            throw `pinNameTest error (pinName): ${pin}`;
+          }
+          if (!numberTest(e[1].trim())) {
+            throw `numberTest error (targetVal): ${e[1].trim()}`;
+          }
           data[e[0].trim()] = Number(e[1]);
         });
         return data;
@@ -41,10 +55,14 @@ http.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
 
 io.on('connection', (socket) => {
   socket.on('exec', (data) => {
-    const tasks = parser(data);
-    console.log(tasks);
-    executer.setCmds(tasks);
-    io.to(socket.id).emit('info', '実行準備完了！');
+    try {
+      const tasks = parser(data);
+      console.log(tasks);
+      executer.setCmds(tasks);
+      io.to(socket.id).emit('info', '実行準備完了！');
+    } catch (e) {
+      io.to(socket.id).emit('error', e);
+    }
   });
   socket.on('funcButton', (index) => {
     console.log(`exec :${index}`, executer.finish);
