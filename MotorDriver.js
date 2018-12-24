c50cb63ae696bf100418ff5dabee88f2dd80cc25
Fukusan64 <ikishikawa64@gmail.com>
2018-12-24 14:44:06 +0900

    MotorDriverClassからGPIOPinを動かせるようにした

--- MotorDriver.js
+++ MotorDriver.js
@@ -1,7 +1,11 @@
+const { Gpio } = require('pigpio');
+
 module.exports = class MotorDriver {
   constructor([plusPin, minusPin], deltaT) {
-    this.plusPin = plusPin;
-    this.minusPin = minusPin;
+    this.plusPin = new Gpio(plusPin, {mode: Gpio.OUTPUT});
+    this.minusPin = new Gpio(minusPin, {mode: Gpio.OUTPUT});
+    this.plusPinNum = plusPin;
+    this.minusPinNum = minusPin;
     this.deltaD = 0;
     this.d = 0;
     this.targetD = 0;
@@ -17,11 +21,15 @@ module.exports = class MotorDriver {
       this.d = this.targetD;
       console.group(`d=${this.d}`);
       if (this.d < 0) {
-        console.log(this.minusPin, Math.round(Math.abs(this.d)));
-        console.log(this.plusPin, 0);
+        console.log(this.minusPinNum, Math.round(Math.abs(this.d)));
+        this.minusPin.pwmWrite(Math.round(Math.abs(this.d)));
+        console.log(this.plusPinNum, 0);
+        this.plusPin.pwmWrite(0);
       } else {
-        console.log(this.minusPin, 0);
-        console.log(this.plusPin, Math.round(Math.abs(this.d)));
+        console.log(this.minusPinNum, 0);
+        this.minusPin.pwmWrite(0);
+        console.log(this.plusPinNum, Math.round(Math.abs(this.d)));
+        this.plusPin.pwmWrite(Math.round(Math.abs(this.d)));
       }
       console.groupEnd();
       this.finish = true;
@@ -29,11 +37,17 @@ module.exports = class MotorDriver {
     } else {
       console.group(`d=${this.d}`);
       if (this.d < 0) {
-        console.log(this.minusPin, Math.round(Math.abs(this.d)));
-        console.log(this.plusPin, 0);
+        const val = Math.round(Math.abs(this.d));
+        console.log(this.minusPinNum, val);
+        this.minusPin.pwmWrite(val);
+        console.log(this.plusPinNum, 0);
+        this.plusPin.pwmWrite(0);
       } else {
-        console.log(this.minusPin, 0);
-        console.log(this.plusPin, Math.round(Math.abs(this.d)));
+        const val = Math.round(Math.abs(this.d));
+        console.log(this.minusPinNum, 0);
+        this.minusPin.pwmWrite(0);
+        console.log(this.plusPinNum, val);
+        this.plusPin.pwmWrite(val);
       }
       console.groupEnd();
       this.finish = false;
@@ -55,7 +69,9 @@ module.exports = class MotorDriver {
     this.updateCount = 0;
     this.deltaD = 0;
     this.d = 0;
-    console.log(this.minusPin, 0);
-    console.log(this.plusPin, 0);
+    console.log(this.minusPinNum, 0);
+    this.minusPin.pwmWrite(0);
+    console.log(this.plusPinNum, 0);
+    this.plusPin.pwmWrite(0);
   }
 }
