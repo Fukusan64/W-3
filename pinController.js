001dd25c69330dec03bff226891aeb7a21b616d0
Fukusan64 <ikishikawa64@gmail.com>
2018-12-30 23:47:29 +0900

    操作できる人を1名に限定するようにした

--- pinController.js
+++ pinController.js
@@ -6,28 +6,44 @@ module.exports = (http) => {
   const executer = new Executer(deltaTmsec);
 
   io.on('connection', (socket) => {
-    socket.on('exec', (data) => {
-      try {
-      const [pinMap, tasks] = parser(data);
-      console.log(pinMap);
-      executer.setPins([[23, 24], [25, 26], ...pinMap]);
-      console.log(tasks);
-      executer.setCmds(tasks);
-      io.to(socket.id).emit('info', '実行準備完了！');
-      } catch (e) {
-      console.error(e);
-      io.to(socket.id).emit('error', e);
-      }
-    });
-    socket.on('funcButton', (index) => {
-      console.log(`exec :${index}`, executer.finish);
-      if (executer.finish)executer.exec(index);
-      else {
-      io.to(socket.id).emit('warn', 'コマンド実行中はボタンを押せません');
-      }
-    });
-    socket.on('edit', () => {
-      executer.stop();
-    });
+    if (socket.client.conn.server.clientsCount > 1) {
+      console.log(`disconnect client[${socket.id}]`);
+      socket.emit(
+        'warn',
+        '先に接続したユーザがいます、5秒後に切断します。時間をおいて接続してください',
+      );
+      setTimeout(() => {
+        socket.emit(
+          'warn',
+          '先に接続したユーザがいます、3秒後に切断します。時間をおいて接続してください',
+        );
+      }, 2000);
+      setTimeout(() => socket.disconnect(true), 5000);
+    } else {
+      socket.on('exec', (data) => {
+        try {
+          const [pinMap, tasks] = parser(data);
+          console.log(pinMap);
+          executer.setPins([[23, 24], [25, 26], ...pinMap]);
+          console.log(tasks);
+          executer.setCmds(tasks);
+          socket.emit('info', '実行準備完了！');
+        } catch (e) {
+          console.error(e);
+          socket.emit('error', e);
+        }
+      });
+      socket.on('funcButton', (index) => {
+        console.log(`exec :${index}`, executer.finish);
+        if (executer.finish) {
+          executer.exec(index);
+        } else {
+          socket.emit('warn', 'コマンド実行中はボタンを押せません');
+        }
+      });
+      socket.on('edit', () => {
+        executer.stop();
+      });
+    }
   });
 };
